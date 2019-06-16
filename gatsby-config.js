e110f9a198801312444383760bab618e9c5e2a45
Marco Acierno <marcoaciernoemail@gmail.com>
2019-06-17 00:43:44 +0100

    Update description, package, and app name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Python Pizza`,
-    description: `AAA`,
+    description: `Python Pizza is a 1 day micro-conference organised around the world to unite the love of Pizza and Python`,
     author: `@pythonpizzaconf`,
   },
   plugins: [
@@ -22,6 +22,7 @@ module.exports = {
         short_name: `python.pizza`,
         start_url: `/?homepage=1`,
         background_color: `#fff`,
+        application_name: 'Python Pizza',
         theme_color: `#ed4337`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
